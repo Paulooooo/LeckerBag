f6668ba02c8c853421e85ac8d4f0dceae4fac624
Pauloooo <paul.kozakiewicz@stud-mail.uni-wuerzburg.de>
2015-06-01 15:32:27 +0200

    Impressum + Startseite v1.0

--- LeckerBag/WebContent/js/functions.js
+++ LeckerBag/WebContent/js/functions.js
@@ -54,4 +54,32 @@ $(function() {
 	    });
 });
 
+setCarouselHeight('#carousel-startseite');
+function setCarouselHeight(id)
+{
+    var slideHeight = [];
+    $(id+' .item').each(function()
+    {
+        // add all slide heights to an array
+        slideHeight.push($(this).height());
+    });
+
+    // find the tallest item
+    max = Math.max.apply(null, slideHeight);
+
+    // set the slide's height
+    $(id+' .item').each(function()
+    {
+        $(this).css('height',max+'px');
+    });
+    $(id+' #startseite_slider_arrow_left').each(function()
+    {
+        $(this).css('padding-top',max/2+'px');
+    });
+    $(id+' #startseite_slider_arrow_right').each(function()
+    {
+        $(this).css('padding-top',max/2+'px');
+    });
+}
+
 
