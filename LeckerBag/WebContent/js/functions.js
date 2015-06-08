7ec15308eba826c9676651330d100f8d09f0cdf8
Pauloooo <paul.kozakiewicz@stud-mail.uni-wuerzburg.de>
2015-06-08 11:19:43 +0200

    Update nach Besprechung

--- LeckerBag/WebContent/js/functions.js
+++ LeckerBag/WebContent/js/functions.js
@@ -82,4 +82,45 @@ function setCarouselHeight(id)
     });
 }
 
+var editProfilKunde = function (value){
+	var inputs = document.getElementsByClassName("profilTextfield");
+	var inputs_collapse = document.getElementsByClassName("profilTextfieldCollapse");
+	var inputs_size = document.getElementsByClassName("profil_element");
+	
+	for(var i = 0; i < inputs.length; i++) {
+	    inputs[i].disabled = value;
+	}
+	if (value){
+		document.getElementById("btn_profil_kunde_edit").style.display="";
+		document.getElementById("btn_profil_kunde_save").style.display="none";
+		for(var j = 0; j < inputs_collapse.length; j++) {
+		    inputs_collapse[j].style.display="none";
+		}
+		for(var g = 0; g < inputs_size.length; g++) {
+			inputs_size[g].style.height="300px";
+		}
+	}
+	else{
+		document.getElementById("btn_profil_kunde_edit").style.display="none";
+		document.getElementById("btn_profil_kunde_save").style.display="";
+		for(var k = 0; k < inputs_collapse.length; k++) {
+		    inputs_collapse[k].style.display="";
+		}
+		for(var h = 0; h < inputs_size.length; h++) {
+			inputs_size[h].style.height="400px";
+		}
+	}
+};
+
+var init = function (){
+	var screenWidth = screen.width;
+	document.getElementById("#startseite_c3").style.width="10000px";
+};
+/*document.addEventListener("DOMContentLoaded", function(){
+     init();
+}, false);
+window.onload = function(){
+    init();
+};*/
+
 
