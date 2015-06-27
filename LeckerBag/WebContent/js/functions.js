1bef36ef6aa0959984042632f276e8e175b48d46
Pauloooo <paul.kozakiewicz@stud-mail.uni-wuerzburg.de>
2015-06-27 15:16:46 +0200

    weiter...

--- LeckerBag/WebContent/js/functions.js
+++ LeckerBag/WebContent/js/functions.js
@@ -1,64 +1,20 @@
 //**** ---- Funktionen load on init  ---- ****
 //--------------------------------------------
-
 //**** Navbar stick on Top ****
 $(function() {
-	    var nav = $('#navigation_bar');
-	    var navButtons = $('.nav_item');
-	    var navButtonLast = $('.nav_item_last'); 
-	    var navbarContainer = $('.navbar_container'); 
-	    var navHomeY = nav.offset().top;
-	    var isFixed = false;
-	    var $w = $(window);
-	    
-	    $w.scroll(function() {
-	        var scrollTop = $w.scrollTop();
-	        var shouldBeFixed = scrollTop > navHomeY;
-	        if (shouldBeFixed && !isFixed) {
-	            nav.css({
-	                position: 'fixed',
-	                top: 0,    
-	                left: nav.offset().left+15, //add 15px padding
-	                width: nav.width(),
-	            });
-	            navButtons.css({
-	            	borderBottom: '1px solid #ddd'
-	            });
-	            navButtonLast.css({
-	            	borderBottom: '1px solid #ddd'
-	            });
-	            navbarContainer.css({
-	                boxShadow: '0px 1px 2px 0px rgba(221,221,221,1)'
-	            });
-	            isFixed = true;
-	        }
-	        else if (!shouldBeFixed && isFixed)
-	        {
-	            nav.css({   
-	                position: '',
-	                top: '',
-		            width: '100%',
-		           // backgroundColor: ''
-	            });
-	            navButtons.css({
-	            	borderBottom: ''
-	            });
-	            navButtonLast.css({
-	            	borderBottom: ''
-	            });
-	            navbarContainer.css({
-	                boxShadow: ''
-	            });
-	            isFixed = false;
-	        }
-	    });
+    $('#nav-wrapper').height($("#nav").height());
+    
+    $('#nav').affix({
+        offset: { top: $('#nav').offset().top }
+    });
 });
-
 
 //**** Navigationsleiste - onResize anpassen ****
 $(window).on('resize', function(){
 	$('.nav_item').css({width: $('.wrapper').width()/6});
 });
+
+$('.nav_item').css({width: $('.wrapper').width()/6});
 //**** Navigationsleiste - Navigationselemente aktivieren, deaktivieren ****
 var setNav = function(site){
 	var navitems = document.getElementsByClassName("nav_item");
